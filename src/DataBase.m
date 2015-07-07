dbbbd9f9f60a6d88719490e53209bd0e9da9255b
honorat <honoratquinard@gmail.com>
2015-07-07 23:58:11 +0200

    Choice of the Operating System in DataBase

--- src/DataBase.m
+++ src/DataBase.m
@@ -1,4 +1,17 @@
 
+% Construct a questdlg with two options
+choice = questdlg('Hello, please choose your OS?', ...
+	'Operating System', ...
+	'Mac','Windows','');
+% Handle response
+global os
+switch choice
+    case 'Mac'
+        os = 1;
+    case 'Windows'
+        os = 2;
+end
+
   % Create the data insolation to plot.
    SampleTime=0.1;
    xdiscretized=1:SampleTime:24;
@@ -7,8 +20,52 @@
    Ins_Intermediate=Insolation_data*4583;
    Ins_Winter=Insolation_data*4888;
    
-% Import data from the files 
+   % Data Link for Simulink Insolation
+   VARins1 = [xdiscretized;Ins_Monsoon];
+   VARins2 = [xdiscretized;Ins_Intermediate];
+   VARins3 = [xdiscretized;Ins_Winter];
+   os
+   if os==1
+% Import data from the files for Mac
 
+    customerAllMonsoon=importdata('dataBase/PowerAllMonsoon.mat'); %  Data from excel Block All
+    customerAllIntermediate=importdata('dataBase/PowerAllIntermediate.mat'); %  Data from excel Block All
+    customerAllWinter=importdata('dataBase/PowerAllWinter.mat'); %  Data from excel Block All
+
+    customerDMonsoon=importdata('dataBase/PowerDMonsoon.mat'); %  Data from excel Block D
+    customerDIntermediate=importdata('dataBase/PowerDIntermediate.mat'); %  Data from excel Block D
+    customerDWinter=importdata('dataBase/PowerDWinter.mat'); %  Data from excel Block D
+
+    customerEMonsoon=importdata('dataBase/PowerEMonsoon.mat'); %  Data from excel Block E
+    customerEIntermediate=importdata('dataBase/PowerEIntermediate.mat'); %  Data from excel Block E
+    customerEWinter=importdata('dataBase/PowerEWinter.mat'); %  Data from excel Block E
+
+    customerCMonsoon=importdata('dataBase/PowerCMonsoon.mat'); %  Data from excel Block C
+    customerCIntermediate=importdata('dataBase/PowerCIntermediate.mat'); %  Data from excel Block C
+    customerCWinter=importdata('dataBase/PowerCWinter.mat'); %  Data from excel Block C
+
+    customerAMonsoon=importdata('dataBase/PowerAMonsoon.mat'); %  Data from excel Block A
+    customerAIntermediate=importdata('dataBase/PowerAIntermediate.mat'); %  Data from excel Block A
+    customerAWinter=importdata('dataBase/PowerAWinter.mat'); %  Data from excel Block A
+
+    customerBMonsoon=importdata('dataBase/PowerBMonsoon.mat'); %  Data from excel Block B
+    customerBIntermediate=importdata('dataBase/PowerBIntermediate.mat'); %  Data from excel Block B
+    customerBWinter=importdata('dataBase/PowerBWinter.mat'); %  Data from excel Block B
+
+    customerLHMonsoon=importdata('dataBase/PowerLHMonsoon.mat'); %  Data from excel Block LH
+    customerLHIntermediate=importdata('dataBase/PowerLHIntermediate.mat'); %  Data from excel Block LH
+    customerLHWinter=importdata('dataBase/PowerLHWinter.mat'); %  Data from excel Block LH
+
+    customerRHMonsoon=importdata('dataBase/PowerRHMonsoon.mat'); %  Data from excel Block RH
+    customerRHIntermediate=importdata('dataBase/PowerRHIntermediate.mat'); %  Data from excel Block RH
+    customerRHWinter=importdata('dataBase/PowerRHWinter.mat'); %  Data from excel Block RH
+
+    Time=importdata('dataBase/SampleT.mat');     % SampleT.mat Data from excel 
+    
+  % Import data from the files for Window  
+  
+   elseif os==2
+       
     customerAllMonsoon=importdata('dataBase\PowerAllMonsoon.mat'); %  Data from excel Block All
     customerAllIntermediate=importdata('dataBase\PowerAllIntermediate.mat'); %  Data from excel Block All
     customerAllWinter=importdata('dataBase\PowerAllWinter.mat'); %  Data from excel Block All
@@ -43,6 +100,7 @@
 
     Time=importdata('dataBase\SampleT.mat');     % SampleT.mat Data from excel 
     
+   end
     
     % linear regression with a polynomial approach for the Active power of the load  
     
@@ -511,23 +569,23 @@ r=1;
     
     
 % Send the Power from the selected block to the simulink
-% VAR1= [xdiscretized;ActivePower_data]; % Active power
-% VAR2= [xdiscretized;ActivePower_data/10];% Reactive power
-% 
-% % Temperature for each seasons from the edit box
-% Temp1=zeros(1,(23/SampleTime)+1);% Creation matrix
-% Temp1(:,:)=26.8; % Set value into the matrix
-% 
-% Temp2=zeros(1,(23/SampleTime)+1);% Creation matrix
-% Temp2(:,:)=17.4; % Set value into the matrix
-% 
-% Temp3=zeros(1,(23/SampleTime)+1);% Creation matrix
-% Temp3(:,:)=23.4; % Set value into the matrix
-% 
-% % Data Link for Simulink Temperature
-% VARTemp1 = [xdiscretized;Temp1];% Monsoon
-% VARTemp2 = [xdiscretized;Temp2];% Intermediate Season
-% VARTemp3 = [xdiscretized;Temp3];% Winter
-   
+VAR1= [xdiscretized;newyfitdiscretized]; % Active power
+VAR2= [xdiscretized;newyfitdiscretized/10];% Reactive power
+
+% Temperature for each seasons from the edit box
+Temp1=zeros(1,(23/SampleTime)+1);% Creation matrix
+Temp1(:,:)=26.8; % Set value into the matrix
+
+Temp2=zeros(1,(23/SampleTime)+1);% Creation matrix
+Temp2(:,:)=17.4; % Set value into the matrix
+
+Temp3=zeros(1,(23/SampleTime)+1);% Creation matrix
+Temp3(:,:)=23.4; % Set value into the matrix
+
+% Data Link for Simulink Temperature
+VARTemp1 = [xdiscretized;Temp1];% Monsoon
+VARTemp2 = [xdiscretized;Temp2];% Intermediate Season
+VARTemp3 = [xdiscretized;Temp3];% Winter
+
      
 run('Supervision_Rev3'); % Run the GUI
